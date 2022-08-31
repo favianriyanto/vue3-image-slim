0340f0ec87c95d88b90f0d3003624825cecf49f6
Favian <favianriyanto@outlook.com>
2022-08-31 15:19:56 +0700

    .

--- src/vue3-image-slim.vue
+++ src/vue3-image-slim.vue
@@ -4,15 +4,31 @@
       type="file"
       :id="inputId"
       style="display: none"
-      accept=".jpg,.jpeg,.png"
+      accept="image/*"
       @change="onSelectFile()"
     />
-    <div
+    <input
+        type="file"
+        id="inputcamera"
+        style="display: none"
+        accept="image/*"
+        capture="camera"
+        @change="onSelectFile()"
+    />
+    <button
       @click="doClick"
-      class="vue-image-slim-button"
+      style="width: 300px; margin: 10px; background-repeat: no-repeat; background-image: url('data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20class%3D%22ionicon%22%20viewBox%3D%220%200%20512%20512%22%3E%3Ctitle%3EImage%3C%2Ftitle%3E%3Cpath%20d%3D%22M416%2064H96a64.07%2064.07%200%2000-64%2064v256a64.07%2064.07%200%200064%2064h320a64.07%2064.07%200%200064-64V128a64.07%2064.07%200%2000-64-64zm-80%2064a48%2048%200%2011-48%2048%2048.05%2048.05%200%200148-48zM96%20416a32%2032%200%2001-32-32v-67.63l94.84-84.3a48.06%2048.06%200%200165.8%201.9l64.95%2064.81L172.37%20416zm352-32a32%2032%200%2001-32%2032H217.63l121.42-121.42a47.72%2047.72%200%200161.64-.16L448%20333.84z%22%2F%3E%3C%2Fsvg%3E')"
       :class="{ disabled }"
-      :style="{ width: btnWidth, height: btnHeight }"
-    ></div>
+      :style="[{ width : btnWidth, height : btnHeight}, hiddenBtn ? {'display' : 'none'} : {'display' : ''}]"
+    ></button>
+
+    <button
+        @click="doClick('camera')"
+        style="width: 50%; background-repeat: no-repeat; background-image: url('data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20class%3D%22ionicon%22%20viewBox%3D%220%200%20512%20512%22%3E%3Ctitle%3ECamera%3C%2Ftitle%3E%3Ccircle%20cx%3D%22256%22%20cy%3D%22272%22%20r%3D%2264%22%2F%3E%3Cpath%20d%3D%22M432%20144h-59c-3%200-6.72-1.94-9.62-5l-25.94-40.94a15.52%2015.52%200%2000-1.37-1.85C327.11%2085.76%20315%2080%20302%2080h-92c-13%200-25.11%205.76-34.07%2016.21a15.52%2015.52%200%2000-1.37%201.85l-25.94%2041c-2.22%202.42-5.34%205-8.62%205v-8a16%2016%200%2000-16-16h-24a16%2016%200%2000-16%2016v8h-4a48.05%2048.05%200%2000-48%2048V384a48.05%2048.05%200%200048%2048h352a48.05%2048.05%200%200048-48V192a48.05%2048.05%200%2000-48-48zM256%20368a96%2096%200%201196-96%2096.11%2096.11%200%2001-96%2096z%22%2F%3E%3C%2Fsvg%3E')"
+        :class="{ disabled }"
+        :style="[{ width : btnWidth, height : btnHeight}, hiddenBtn2 ? {'display' : 'none'} : {'display' : ''}]"
+    ></button>
+
     <div style="display: none">
       <img :id="imgId" />
       <canvas :id="cvsId"></canvas>
@@ -27,6 +43,7 @@ type NullableInput = File | null;
 
 const id: number = new Date().valueOf();
 let input: HTMLInputElement | null,
+  inputcamera: HTMLInputElement | null,
   image: HTMLImageElement | null,
   canvas: HTMLCanvasElement | null,
   ctx: any;
@@ -57,12 +74,27 @@ export default /*#__PURE__*/ defineComponent({
     btnWidth: {
       //按钮宽度
       type: String,
-      default: "100px",
+      default: "70px",
     },
     btnHeight: {
       //按钮高度
       type: String,
-      default: "100px",
+      default: "70px",
+    },
+    disabled2: {
+      //按钮高度
+      type: Boolean,
+      default: false,
+    },
+    hiddenBtn: {
+      //按钮高度
+      type: Boolean,
+      default: false,
+    },
+    hiddenBtn2: {
+      //按钮高度
+      type: Boolean,
+      default: false,
     },
   },
   emits: ["getDataURL", "getFile"],
@@ -76,6 +108,7 @@ export default /*#__PURE__*/ defineComponent({
     const cvsId = computed(() => "cvs" + id);
     onMounted(() => {
       input = <HTMLInputElement>document.getElementById(inputId.value);
+      inputcamera = <HTMLInputElement>document.getElementById('inputcamera');
       image = <HTMLImageElement>document.getElementById(imgId.value);
       if (!input || !image) {
         return;
@@ -111,10 +144,17 @@ export default /*#__PURE__*/ defineComponent({
       };
     });
 
-    const doClick = () => {
-      if (input && !props.disabled) {
-        input.click();
+    const doClick = (type : string) => {
+      if (type !== 'camera'){
+        if (input && !props.disabled) {
+          input.click();
+        }
+      }else{
+        if (inputcamera && !props.disabled) {
+          inputcamera.click()
+        }
       }
+
     };
 
     const onSelectFile = async () => {
@@ -212,7 +252,7 @@ export default /*#__PURE__*/ defineComponent({
 <style scope>
 .vue-image-slim-button {
   position: relative;
-  border: 1px solid #eee;
+  border: 3px solid #575757;
   background: transparent !important;
   display: inline-block;
   cursor: pointer;
@@ -225,7 +265,7 @@ export default /*#__PURE__*/ defineComponent({
 .vue-image-slim-button:after {
   content: "";
   position: absolute;
-  background: #ccc;
+  background: #575757;
 }
 
 .vue-image-slim-button:before {
